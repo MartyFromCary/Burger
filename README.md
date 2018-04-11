80b88d568096218ef0a28ed04514405a55c0c4a8
Elias A Martinez <MartyFromCary@gmail.com>
2018-04-11 11:41:18 -0400

    Clean Up

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Handlebars
+
+Burger App with express-handlebars and MySql
+Intro to Model–view–controller
--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     port: 3306,
-    host: "l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",  //"localhost",
-    user: "oh5tse4622r0r9ww",                                           //"root",
-    password: "nqaplsizvhiiprrh",                                       //"Norma123$",
-    database: "burgers_db"
+    host: "l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "oh5tse4622r0r9ww",
+    password: "nqaplsizvhiiprrh",
+    database: "z4fe6hrk7f0vdoox"
   });
 }
 
